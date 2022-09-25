5a720af96103610fb4914e67f61f232fcdc7bcbe
Ricardo Gil <rgv1366@hotmail.com>
2022-09-25 17:29:10 +0200

    Nueva versión

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define DEBUG
+#define DEBUG_OFF
 
 #include <Arduino.h>
 #include <pines.h>
@@ -13,7 +13,6 @@ int temperatura = 0;
 
 void setup()
 {
-  delay(10000);
   // Configuración inicial.
 #if defined(DEBUG)
   Serial2.begin(115200);
@@ -23,19 +22,9 @@ void setup()
   pinMode(TERMOMETRO_POWER, OUTPUT);
   digitalWrite(TERMOMETRO_POWER, 0);
 
-  pinMode(TEMP_DONEPIN, OUTPUT);
-  digitalWrite(TEMP_DONEPIN, LOW);
+  pinMode(TPL5010_DONE, OUTPUT);
 
-  // LoRa.setPins(RFM95_SS, RFM95_RST, RFM95_DIO0);
-  LoRa.setPins(LORA_SSPIN, LORA_RESETPIN, LORA_DIO0PIN);
-
-#if defined(DEBUG)
-  Serial2.println(F("Configuramos modo sueño."));
-#endif
-
-  // Configuramos el modo "sueño"
-  // set_sleep_mode(SLEEP_MODE_STANDBY);
-  // sleep_enable();
+  LoRa.setPins(RFM95_SS, RFM95_RST, RFM95_DIO0);
 
   // Módulo lora
   if (!LoRa.begin(FRECUENCIA))
@@ -44,16 +33,9 @@ void setup()
     Serial2.println(F("Fallo al inicalizar el módulo de rádio RFM_95."));
 #endif
   }
-  else
-  {
-    LoRa.beginPacket();
-    LoRa.print(F("Modulos LoRa inicializado."));
-    LoRa.endPacket();
-  }
 
   // Activamos la interrupción que tiene que despertarmos.
   attachInterrupt(digitalPinToInterrupt(TPL5010_WAKE), Despierta, FALLING);
-  // attachInterrupt(TEMP_WAKEPIN, Despierta, RISING);
 }
 
 void loop()
@@ -66,23 +48,29 @@ void loop()
   LoRa.print(F("Estamos depiertos."));
   LoRa.endPacket();
 #endif
-  digitalWrite(TPL5010_DONE, LOW);
 
   // Emcendemos el termómetro
   digitalWrite(TERMOMETRO_POWER, HIGH);
-  LoRa.begin(FRECUENCIA);
+
+  // Finalizamos el aviso al temporizador.
+  digitalWrite(TPL5010_DONE, LOW);
+
   // Encendemos el módulo de radio.
   if (!LoRa.begin(FRECUENCIA))
   {
-    Serial2.print(F("¡ FALLO ! ---> "));
-  }
 #if defined(DEBUG)
-  Serial2.println(F("LoRa activado."));
-  LoRa.beginPacket();
-  LoRa.print(F("LoRa activado."));
-  LoRa.endPacket();
+    Serial2.print(F("¡ FALLO ! ---> "));
 #endif
-
+  }
+  else
+  {
+#if defined(DEBUG)
+    Serial2.println(F("LoRa activado."));
+    LoRa.beginPacket();
+    LoRa.print(F("LoRa activado."));
+    LoRa.endPacket();
+#endif
+  }
   // Leemos la temperatura.
   temperatura = analogRead(TERMOMETRO_DATOS);
 
@@ -98,8 +86,9 @@ void loop()
   LoRa.beginPacket();
   LoRa.print(F("Información enviada. ¡ A dormir !"));
   LoRa.endPacket();
-#endif
+  // Ponemos un retraso de 5 milisegundos para que de tiempo de escribir en el monitor serial.
   delay(5);
+#endif
   // y ponemos a dormir el módulo de rádio.
   LoRa.sleep();
 
